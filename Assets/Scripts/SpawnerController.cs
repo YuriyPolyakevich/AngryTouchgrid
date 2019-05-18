a59c9cb7b09dba44dbb3463c69426b59e35814ae
Yuriy Polyakevich <yuriy.polyakevich@tensor.technology>
2019-05-18 17:29:35 +0300

    Arrow control, spawn added,  temporary prefabs,

---
+++ Assets/Scripts/SpawnerController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class SpawnerController : MonoBehaviour
+{
+
+	public GameObject[] spawnObjects;
+
+
+	private void Start ()
+	{
+		SpawnObjects();
+	}
+
+	private void SpawnObjects()
+	{
+		foreach (var spawnObject in spawnObjects)
+		{
+			Instantiate(spawnObject);
+		}
+	}
+
+}
