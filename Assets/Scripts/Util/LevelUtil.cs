1806815021b46f6c367f539f0272ba46c2e9cadd
Yuriy Polyakevich <yuriy.polyakevich@tensor.technology>
2019-05-20 22:55:45 +0300

    levelUtil

--- Assets/Scripts/Util/LevelUtil.cs
+++ Assets/Scripts/Util/LevelUtil.cs
@@ -1,7 +1,26 @@
-namespace a
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Util
 {
-    public class LevelUtil
+    public class LevelUtil : MonoBehaviour
     {
-        
+        private static int CurrentLevel { get; set; }
+        public Scene[] Scenes;
+
+        private void Start()
+        {
+            CurrentLevel = 1;
+        }
+
+        public static void LoadNextLevel()
+        {
+            SceneManager.LoadScene(GetSceneName());
+        }
+
+        private static string GetSceneName()
+        {
+            return "1";
+        }
     }
 }
