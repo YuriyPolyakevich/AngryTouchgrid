a726df5c1c0d5508153f09a424d59a2ecab9c1ca
Yuriy Polyakevich <yuriy.polyakevich@tensor.technology>
2019-05-30 23:14:39 +0300

    boot trajectory added

--- Assets/Scripts/Controller/BootController.cs
+++ Assets/Scripts/Controller/BootController.cs
@@ -1,18 +1,28 @@
 ﻿using System;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace Controller
 {
     public class BootController : MonoBehaviour
     {
+        [FormerlySerializedAs("_trajectoryDotPrefab")]
+        public GameObject trajectoryDotPrefab;
+
+        private const float DotTimeStep = 0.05f;
         private bool _isDragging = false;
         private Vector3 _firstPosition = Vector3.zero;
         private Vector3 _lastPosition = Vector3.zero;
         private Rigidbody _rigidBody = null;
-        private const int Force = 2000;
+        private const int Force = 70;
         private Camera _camera;
         private GoalController _goalController;
-        public Vector3 InitialPosition { get; private set; }
+        private Vector3 InitialPosition { get; set; }
+        private const int NumOfDotsToShow = 20;
+        private readonly List<GameObject> _dotPrefabs = new List<GameObject>();
+        private readonly Vector3 _gravityVector = new Vector3(0f, -100f, 0);
+        private bool _isBootMoving = false;
 
         private void Start()
         {
@@ -32,6 +42,7 @@ namespace Controller
                 if (!_isDragging)
                 {
                     StartDraggingConfiguration(touch);
+                    InstantiateDots();
                 }
                 else
                 {
@@ -41,8 +52,25 @@ namespace Controller
             else
             {
                 if (!_isDragging) return;
+                ClearDots();
                 MoveObject();
                 _isDragging = false;
+            }
+        }
+
+        private void InstantiateDots()
+        {
+            for (var i = 0; i < NumOfDotsToShow; i++)
+            {
+                var trajectoryDot = Instantiate(trajectoryDotPrefab);
+                _dotPrefabs.Add(trajectoryDot);
+            }
+        }
+
+        public void FixedUpdate() {
+            if (_isBootMoving)
+            {
+                _rigidBody.AddForce(_gravityVector, ForceMode.Acceleration);
             }
         }
 
@@ -50,19 +78,41 @@ namespace Controller
         {
             RaycastHit raycastHit;
             var ray = _camera.ScreenPointToRay(touch.position);
-            return Physics.Raycast(ray, out raycastHit) && raycastHit.transform.gameObject.tag.Equals("Player");;
+            return Physics.Raycast(ray, out raycastHit) && raycastHit.transform.gameObject.tag.Equals("Player");
         }
 
         private void MoveObject()
         {
-            var moveDirection = -(_lastPosition - _firstPosition);
-            _rigidBody.AddForce(moveDirection * Force, ForceMode.Force);
+            var velocityVector = CalculateForce();
+            var velocitySpeed =
+                Mathf.Sqrt(velocityVector.x * velocityVector.x + velocityVector.y * velocityVector.y);
+            _isBootMoving = true; 
+            _rigidBody.AddForce(CalculateForce(), ForceMode.Impulse);
+//            _rigidBody.velocity = CalculateVelocity();
             _goalController.BootKickedTime = DateTime.Now;
+        }
+
+        private Vector3 CalculateForce()
+        {
+            var calculateVelocity = -(_lastPosition - _firstPosition) * Force;
+            return calculateVelocity;
         }
 
         private void StopDraggingConfiguration(Touch touch)
         {
             _lastPosition = touch.position;
+            for (var i = 0; i < _dotPrefabs.Count; i++)
+            {
+                _dotPrefabs[i].transform.position = CalculatePosition( DotTimeStep * i);
+            }
+        }
+
+        private void ClearDots()
+        {
+            foreach (var i in _dotPrefabs)
+            {
+                Destroy(i);
+            }
         }
 
         private void StartDraggingConfiguration(Touch touch)
@@ -70,6 +120,18 @@ namespace Controller
             if (!IsHitted(touch)) return;
             _isDragging = true;
             _firstPosition = touch.position;
+        }
+
+        private Vector3 CalculatePosition(float elapsedTime)
+        {
+//            var calculatePosition = _gravityVector * elapsedTime * elapsedTime * 0.5f +
+//                                    CalculateVelocityVector() * elapsedTime + InitialPosition;
+            var mass = _rigidBody.mass;
+            var calculatePosition = InitialPosition +
+                                    (CalculateForce() / mass) * elapsedTime
+                                    + _gravityVector * elapsedTime * elapsedTime / 2; 
+                                    
+            return calculatePosition;
         }
     }
 }
